690e43d65c371e8d8c4ce0394cda5fccecf7d6b8
Ibraheem Tarek <ibraheemtarek99@yahoo.com>
2023-10-07 08:20:19 -0700

    display letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Determines either greater than 5, is less than 6, or is 0
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char ch = 'a';
+	while(ch < 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	return (0);
+}
