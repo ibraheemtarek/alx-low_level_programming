90141a721438e75d6ea355db53a77cc5e6d23c21
Ibraheem Tarek <ibraheemtarek99@yahoo.com>
2023-10-07 11:39:06 -0700

    display hexadecimal numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints base 10 numbers
+ *
+ * Return: Always 0 (Succuss)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
