f1fb4aef18cd15924351cd36c47b01a407ed47d7
Ibraheem Tarek <ibraheemtarek99@yahoo.com>
2023-10-07 10:13:33 -0700

    display letters but not "e" "q"

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,14 +11,13 @@ int main(void)
 
 	while (ch <= 'z')
 	{
-		if (ch != 'e'&& ch != 'q')
+		if (ch != 'e' && ch != 'q')
 		{
 			putchar(ch);
 			ch++;
 		}
 	
 	}
-
 	putchar('\n');
 	return (0);
 }
