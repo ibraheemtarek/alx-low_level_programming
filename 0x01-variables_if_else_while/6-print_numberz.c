3d6c8bef4e5c0f288038a19f1898362a2b4418d4
Ibraheem Tarek <ibraheemtarek99@yahoo.com>
2023-10-07 11:14:29 -0700

    display base 10 numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints base 10 numbers
+ *
+ * Return: Always 0 (Succuss)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
