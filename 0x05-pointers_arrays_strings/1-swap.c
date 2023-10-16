71531e15c091da4dd8882b0a28cda86c49f0f473
Ibraheem Tarek <ibraheemtarek99@yahoo.com>
2023-10-16 13:43:07 -0700

    swap values between integrs

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- *swap_int - used to swap between two integers
- *@a: first integer
- *@b: second integer
- *return: no return 
+*swap_int - used to swap between two integers
+*@a: first integer
+*@b: second integer
+*return: no return 
 */
+
 void swap_int(int *a, int *b)
 {
 	int c = *a;
