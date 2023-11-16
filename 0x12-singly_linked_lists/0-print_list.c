a17a095c125cbc09d4f9b0579c3eb82c23d678ee
Ibraheem Tarek <ibraheemtarek99@yahoo.com>
2023-11-16 10:55:36 -0800

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,15 +9,15 @@ size_t print_list(const list_t *h)
 	size_t celem;
 
 	celem = 0;
-	
-	if(h != NULL)
+
+	if (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		celem++;	
+		celem++;
 	}
 	return (celem);
 }
